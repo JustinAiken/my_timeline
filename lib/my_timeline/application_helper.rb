f0b2b383c7690fe8af396e40a022ac39443e1875
Justin Aiken <60tonangel@gmail.com>
2013-12-15 17:14:44 -0700

    Send undefined path/view helpers to the main application context

---
+++ lib/my_timeline/application_helper.rb
@@ -0,0 +1,28 @@
+module MyTimeline
+  module ApplicationHelper
+    def method_missing(method, *args, &block)
+      Rails.logger.fatal "LOOKING FOR ROUTES #{method}"
+      if method.to_s.end_with?('_path') || method.to_s.end_with?('_url')
+        if main_app.respond_to? method
+          main_app.send method, *args
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+
+    def respond_to?(method)
+      if method.to_s.end_with?('_path') || method.to_s.end_with?('_url')
+        if main_app.respond_to? method
+          true
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+  end
+end
